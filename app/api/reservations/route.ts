4d8dae9b304d558eefaafc77f6b36d799447fd95
Saba <khubashvili.saba12@gmail.com>
2023-04-11 19:42:56 +0400

    Fully Done

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -36,5 +36,5 @@ export async function POST(request:Request){
             }
         }
     })
-    return NextResponse.json(listingAndReservation  )
+    return NextResponse.json(listingAndReservation)
 }
